2c00da35c3d0ffbc9437746052f9b32a36df219d
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-17 22:12:57 +0200

    remove print statements

--- controllers/movie_controller.py
+++ controllers/movie_controller.py
@@ -4,7 +4,6 @@ def getRecommendations(movie, rating, get_similar_movies, flash):
         returned_similar = get_similar_movies(movie, rating)
         stringified_df = returned_similar[0:15].to_string()
         stringified_df = stringified_df.split("\n")
-        print(stringified_df)
         movies_array=[]
         for i in stringified_df:
             i = i.split("  ")[0]
